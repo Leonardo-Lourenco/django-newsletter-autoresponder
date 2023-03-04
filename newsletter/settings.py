66570ca950b2801d6d17da272773796b8f72d1ed
Leonardo Lourenco <lnddru@gmail.com>
2023-03-04 17:53:45 -0300

    Add files via upload

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -58,7 +58,8 @@ ROOT_URLCONF = 'newsletter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #'DIRS': [],
+        'DIRS':[str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
