d6c91fd5a5a880e53b4c221e525faf52b638c8ae
Leonardo Lourenco <lnddru@gmail.com>
2023-03-18 11:48:06 -0300

    Add files via upload

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import DashboardHomeView, NewslettersDashboardHomeView, NewsletterCreatedView
+from .views import DashboardHomeView, NewslettersDashboardHomeView, NewsletterCreatedView, NewsletterDetailView, NewsletterUpdateView
 
 app_name="dashboard"
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('',DashboardHomeView.as_view(),  name="home"),
     path('list/',NewslettersDashboardHomeView.as_view(),  name="list"),
     path('created/',NewsletterCreatedView.as_view(),  name="created"),
+    path('detail/<int:pk>',NewsletterDetailView.as_view(),  name="detail"),
+    path('update/<int:pk>',NewsletterUpdateView.as_view(),  name="update"),
     
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView, View
 from aplicacaonewsletter.models import Newsletter
 from aplicacaonewsletter.forms import NewsletterCreationForm
@@ -9,6 +9,8 @@ from django.core.mail import send_mail, EmailMultiAlternatives, EmailMessage
 # Create your views here.
 class DashboardHomeView(TemplateView):
     template_name="dashboard/index.html"
+
+
 
 # Listando todos os emails cadastrados na nossa NewsLetter
 class NewslettersDashboardHomeView(View):
@@ -21,6 +23,8 @@ class NewslettersDashboardHomeView(View):
         }
 
         return render(request, 'dashboard/list.html', context)
+
+
 
 #Botão do Painel cadastar um novo email via painel adm
 class NewsletterCreatedView(View):
@@ -60,5 +64,70 @@ class NewsletterCreatedView(View):
 
         } 
         return render(request, 'dashboard/created.html', context)
-    
+
+
+ 
 # Para visualizar via PAINEL ADM as informações detalhadas dos emails enviados
+class NewsletterDetailView(View):
+
+    def get(self, request,pk,  *args, **kwargs):
+
+        newsletter=get_object_or_404(Newsletter, pk=pk)
+
+        context= {
+            'newsletter': newsletter
+        }
+
+        return render(request, 'dashboard/detail.html')
+    
+
+
+#Botão do Painel EDITAR um email enviado
+class NewsletterUpdateView(View):
+    def get(self, request, *args, **kwargs):
+
+        form=NewsletterCreationForm()
+
+        context= {
+             'form':form
+
+        }      
+        return render(request, 'dashboard/update.html', context)
+
+    # enviando o emails para a pessoa
+    def post(self,request,pk, *args, **kwargs):
+
+        newsletter=get_object_or_404(Newsletter, pk=pk)
+
+        if request.method=="POST":
+
+            form=NewsletterCreationForm(request.POST or None)
+
+            if form.is_valid():
+                instance=form.save()
+                newsletter=Newsletter.objects.get(id=instance.id)
+
+                if newsletter.status=="Published":
+                    subject = newsletter.subject
+                    body = newsletter.body
+                    from_email = settings.EMAIL_HOST_USER
+                    for email in newsletter.email.all():
+                        send_mail(subject=subject, from_email=from_email, recipient_list=[email],
+                                 message=body, fail_silently=True )
+                #caso status publish
+                return redirect('dashboard:detail', pk=newsletter.id)
+            
+             #caso status draft
+            return redirect('dashboard:detail', pk=newsletter.id)
+
+        else:
+
+            form=NewsletterCreationForm(instance=newsletter)
+
+
+
+        context= {
+             'form':form
+
+        } 
+        return render(request, 'dashboard/update.html', context)
